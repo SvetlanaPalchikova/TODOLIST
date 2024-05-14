553462af6340409637469f4ef3017b04aa374b36
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2024-05-14 22:03:38 +0300

    lesson1 time2:46

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const TodoList = () => {
+type TodoListPropsType = {
+    title: string
+}
+
+const TodoList: React.FC<TodoListPropsType> = (props: TodoListPropsType) => {
     return (
         <div>
-            
+            <div className="todolist">
+                <h3>{props.title}</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
         </div>
     );
 };
